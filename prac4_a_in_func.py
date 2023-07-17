f2acd6d1c343737f8814c9d2660b268d861e962c
tisha-aa <tisha.shar13@gmail.com>
2023-07-17 14:19:59 +0530

    added prac4_a_in_func

---
+++ prac4_a_in_func.py
@@ -0,0 +1,32 @@
+
+#list1=[1,2]
+#list2=[1,2,4,5,6,7]
+list1=[]
+list2=[]
+
+n=int(input("Enter the number of elements in List1:"))
+
+print("Enter elements for List1:")
+for i in range(0,n):
+      a=input()
+      list1.append(a)
+print("List1:",list1)
+
+m=int(input("Enter the number of elements in List2:"))
+print("Enter elements for List2:")
+for i in range(0,m):
+      b=input()
+      list2.append(b)
+print("List2:",list2)
+
+
+temp=0
+for i in list1:
+    if i in list2:
+        temp=temp+1
+
+if temp>0:
+    print("TRUE")
+else:
+    print("FALSE")
+        
