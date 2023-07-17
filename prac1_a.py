f3ec0faa97a93d8b81cdfca7b540875ccf4719a1
Tisha Sharma <124681335+tisha-aa@users.noreply.github.com>
2023-07-17 18:19:00 +0530

    added question

--- prac1_a.py
+++ prac1_a.py
@@ -1,3 +1,7 @@
+# Question:
+# Create a program that asks the user to enter their name and their age. Print outa
+# message addressed to them that tells them the year that they will turn 100 years old.
+
 name=input("Enter your name:")
 age=int(input("Enter your age:"))
 n=100-age
