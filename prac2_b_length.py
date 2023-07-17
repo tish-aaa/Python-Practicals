2e7dde2153e090c5fb744bd44468cd5aab67a667
tisha-aa <tisha.shar13@gmail.com>
2023-07-17 14:16:15 +0530

    added prac2_b_length

---
+++ prac2_b_length.py
@@ -0,0 +1,18 @@
+def leng(a):
+    count=0
+    for i in a:
+        count+=1
+    return count
+a=input("Enter a string:")
+print("Length of the string:",leng(a))
+
+l=["pink","barbie","ken"]
+print("List:",l)
+print("Length of the list:",leng(l))
+
+
+
+#a=len(Mylist)
+#print("Length of list Mylist:",a)
+#b='barbie'
+#print("Length of string Barbie:",len(b))
