ba1c6cb77ab001619192b8d212e76e7b9a2d2475
tisha-aa <tisha.shar13@gmail.com>
2023-07-17 14:15:57 +0530

    added prac2_a_vowel

---
+++ prac2_a_vowel.py
@@ -0,0 +1,10 @@
+
+
+def vowel(a):
+    if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+        print("True. It is a vowel.")
+    else:
+        print("False. It is a consonant.")
+
+a=input("Enter a character:")
+vowel(a)
